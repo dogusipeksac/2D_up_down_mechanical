82c603c72ceb7e653e5b31e2690a1ef2cecb3a4e
dogus ipeksac <dogus.ads.1998@hotmail.com>
2021-07-26 18:46:32 +0300

    player and movementJoystick done

---
+++ Assets/Script/MovementJoystick.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovementJoystick : MonoBehaviour
+{
+    public GameObject joystick;
+    public GameObject joystickBG;
+    public Vector2 joystickVektor;
+    private Vector2 joystickTouchPos;
+    private Vector2 joystickOriginalPos;
+    private float joystickRadius;
+
+    bool facingRight = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystickOriginalPos = joystickBG.transform.position;
+        joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 4;
+
+    }
+    public void PointerDown()
+    {
+        joystick.transform.position= Input.mousePosition;
+        joystickBG.transform.position= Input.mousePosition;
+        joystickTouchPos = Input.mousePosition;
+
+
+    }
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector2 dragPos = pointerEventData.position;
+        joystickVektor = (dragPos * joystickTouchPos).normalized;
+        float joystickDist = Vector2.Distance(dragPos,joystickTouchPos);
+        if (joystickDist<joystickRadius)
+        {
+            joystick.transform.position = joystickTouchPos + joystickVektor * joystickDist;
+            
+        }
+        else
+        {
+            joystick.transform.position = joystickTouchPos + joystickVektor * joystickRadius;
+            
+        }
+    }
+    public void PointerUp()
+    {
+        joystickVektor = Vector2.zero;
+        joystick.transform.position = joystickOriginalPos;
+        joystickBG.transform.position = joystickOriginalPos;
+    }
+    void flip(float h)
+    {
+        if (h > 0 && !facingRight || h < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector2 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public MovementJoystick movementJoystick;
+    public float playerSpeed;
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(movementJoystick.joystickVektor.y != 0){
+            rigidbody2D.velocity=new Vector2(movementJoystick.joystickVektor.x*playerSpeed,
+            movementJoystick.joystickVektor.y*playerSpeed);
+        }
+        else{
+            rigidbody2D.velocity=Vector2.zero;
+        }
+
+
+    }
+}
